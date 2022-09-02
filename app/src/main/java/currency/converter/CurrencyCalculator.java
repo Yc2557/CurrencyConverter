e6d75ef29f2ff89d8c72758a9e49dcbcbfb57a6e
Yu Hong <yche7615@uni.sydney.edu.au>
2022-09-02 11:59:48 +1000

    added method calculateAmount and started calculateStatistic

--- app/src/main/java/currency/converter/CurrencyCalculator.java
+++ app/src/main/java/currency/converter/CurrencyCalculator.java
@@ -1,4 +1,17 @@
 package currency.converter;
 
+import java.util.List;
+import java.util.Map;
+
 public class CurrencyCalculator {
+
+    public float calculateAmount(float amount, float rate) {
+        return amount * rate;
+    }
+
+    public Map<String, Float> calculateStatistic(List<Float> historyList) { //takes list of rates
+        //returns map with name of statistic e.g. "mean" as key and the mean as value
+        
+
+    }
 }
