9d869697c982d3c424a529fcd3b64333f43a1060
khou0693 <khou0693@uni.sydney.edu.au>
2022-09-02 11:59:18 +1000

    added Scanner and reading to App.java

--- app/src/main/java/currency/converter/App.java
+++ app/src/main/java/currency/converter/App.java
@@ -3,11 +3,35 @@
  */
 package currency.converter;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         // Check admin
 
         // Input Loop from user
-        Scanner sc = new Scanner(system.in);
+        CurrencyHandler handler = new CurrencyHandler();
+        Scanner sc = new Scanner(System.in);
+
+        String input = "";
+
+        while (sc.hasNextLine()) {
+            input = sc.nextLine();
+
+            // Break up input
+            String[] input_list = input.split(" ");
+            String command = input_list[0];
+
+            switch (command) {
+                case "convert":
+                    System.out.println("Prints");
+                case "exit":
+                    System.out.println("Have a good day!");
+                    sc.close();
+                    break;
+            }
+
+        }
     }
 }
