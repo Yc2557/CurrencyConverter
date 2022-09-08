b7219b5ad4e6882c3964d2c6775f1b598ebbc062
khou0693 <khou0693@uni.sydney.edu.au>
2022-09-09 09:33:01 +1000

    displaytool framework

--- app/src/main/java/currency/converter/App.java
+++ app/src/main/java/currency/converter/App.java
@@ -8,16 +8,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // Check admin
-
-        // Input Loop from user
-        CurrencyHandler handler = new CurrencyHandler(true);
+        // Initialise variables
         Scanner sc = new Scanner(System.in);
-
         String input = "";
-
         Boolean exitFlag = false;
+        Boolean isAdmin = false;
 
+        // Set user to admin if command arg given
+        if (args.length == 0) {
+            isAdmin = false;
+
+        } else if (args[0] == "admin") {
+            isAdmin = true;
+        }
+        CurrencyHandler handler = new CurrencyHandler(isAdmin);
+
+        // Print out entry message
+        DisplayTool.displayTitle();
+
+        // Command Line Loop
         do {
             System.out.println("Please input your command: ");
             input = sc.nextLine();
@@ -30,10 +39,21 @@ public class App {
                 case "convert":
                     System.out.println("Prints");
                     break;
+                case "display":
+                    // Displays popular results
+                
+                case "update":
+                    // Updates dataset
+                case "add":
+                    // Add exchange rate
+                case "summary":
+                    // Print out conversion history of two currencies
                 case "exit":
                     System.out.println("Have a good day!");
                     exitFlag = true;
                     break;
+                case "help":
+                    System.out.println("help menu here.");
                 default:
                     System.out.println("The command you've entered is invalid.");
             }
--- app/src/main/java/currency/converter/CurrencyHandler.java
+++ app/src/main/java/currency/converter/CurrencyHandler.java
@@ -4,16 +4,15 @@ import java.util.List;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 
 public class CurrencyHandler {
-    private boolean isAdmin;
     private CurrencyCalculator currCalc;
     private DatabaseManager DBM;
 
     public CurrencyHandler(boolean admin) {
-        this.isAdmin = admin;
         this.currCalc = new CurrencyCalculator();
-        this.DBM = new DatabaseManager();
+        this.DBM = new DatabaseManager(admin);
     }
 
     public float convertCurrency(float amount, String currCurrency, String newCurrency) {
@@ -32,8 +31,7 @@ public class CurrencyHandler {
                 String toCurrency = popularCurrencies.get(j);
                 if (i == j) {
                     display[i][j] = "-";
-                }
-                else {
+                } else {
                     float conversion = DBM.getConversion(fromCurrency, toCurrency);
                     boolean upDirection = DBM.checkDirection(fromCurrency, toCurrency);
                     if (upDirection) {
@@ -49,37 +47,40 @@ public class CurrencyHandler {
     }
 
     public Boolean addCurrency(String currency) {
-        this.DBM.add(currency); //add new currency to the json
+        this.DBM.add(currency); // add new currency to the json
 
         return Boolean.TRUE;
     }
 
     public Map<String, Float> collateHistoryResults(Map<String, Float> currency, float amount) {
 
-        //get list of rates from map
+        // get list of rates from map
         List<Float> listOfRates = new ArrayList<>(currency.values());
 
-        //might need to do prints here
+        // might need to do prints here
         return currCalc.calculateStatistic(listOfRates);
     }
+
     public void updateCurrency(String curr1, String curr2, float newRate, LocalDate date) {
         LocalDate recentDate = DBM.checkDate(curr1, curr2);
         if (!recentDate.equals(date)) {
-            //can convert date to a string if needed, left as LocalDate
+            // can convert date to a string if needed, left as LocalDate
             DBM.addRate(curr1, curr2, newRate, date);
+        }
+    }
 
     public void printConversionHistory(String curr1, String curr2, String startDate, String endDate) {
         HashMap<String, Float> conversionRates = DBM.getConversionHistory(curr1, curr2, startDate, endDate);
         HashMap<String, Float> statMap = currCalc.calculateStatistic(DBM.getPastConversion());
 
         System.out.println("Conversion Rate History of " + curr1 + " to " + curr2);
-        for (Entry <String,Float> entry : conversionRates) {
+        for (Entry<String, Float> entry : conversionRates) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
         System.out.println("Statistics");
-        for (Entry<String,Float> entry : statMap) {
-            System.out.println(entry.getKey() + ": " +  entry.getValue());
+        for (Entry<String, Float> entry : statMap) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
